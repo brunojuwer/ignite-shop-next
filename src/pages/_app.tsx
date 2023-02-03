dea79845db3e1676a4332862a57fb8400b4b4b80
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-02-03 18:57:05 -0300

    Added a carousel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+import logo from "../assets/logo.svg"
+import Image from "next/image"
+import { Container, Header } from "../styles/pages/app"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logo} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
-
-export default MyApp
