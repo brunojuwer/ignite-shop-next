0f863a458446d9c1968a415851a1e5de880a2313
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-02-06 08:36:02 -0300

    Fixed problem with reload page when click on link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,16 +30,21 @@ export default function Home({ products }: HomeProps) {
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {
-        products.map(product => 
-        (
-          <Product className='keen-slider__slide' key={product.id}>
-            <Image src={product.imageUrl} width={520} height={480} alt=""/>
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
-        ))
+        products.map(product => {
+          return (
+            <Product 
+              className='keen-slider__slide' 
+              href={`/product/${product.id}`} 
+              key={product.id}
+            >
+              <Image src={product.imageUrl} width={520} height={480} alt=""/>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          )
+        })
       }
     </HomeContainer>
   )
