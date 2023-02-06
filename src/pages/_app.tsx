829c05c164820d9dd45d799467bbb6434c3f7361
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-02-06 09:31:10 -0300

    Created product page as SSG

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { globalStyles } from "../styles/global"
 import logo from "../assets/logo.svg"
 import Image from "next/image"
 import { Container, Header } from "../styles/pages/app"
+import Link from "next/link"
 
 globalStyles()
 
@@ -10,7 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logo} alt="" />
+        <Link href={`/`}>
+          <Image src={logo} alt="" />
+        </Link>
       </Header>
       <Component {...pageProps} />
     </Container>
