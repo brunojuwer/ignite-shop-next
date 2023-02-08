7450ea7a8400e28610bbd082fdeff71fa3984249
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-02-08 11:08:17 -0300

    Added radix dialog

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,15 +1,30 @@
 import { Handbag } from "phosphor-react";
 import { CartContainer } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
+
 
 interface CartProps {
     color?: string,
 }
 
 export function Cart({ color }: CartProps) {
-    return (
+  return (
+  
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
         <CartContainer color={color}> 
-            <Handbag size={24} weight="bold"/>
-            <div>1</div>
+          <Handbag size={24} weight="bold"/>
+          <div>1</div>
         </CartContainer>
-    )
+      </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay />
+          <Dialog.Content>
+            <Dialog.Title />
+            <Dialog.Description />
+            <Dialog.Close />
+          </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
 }
